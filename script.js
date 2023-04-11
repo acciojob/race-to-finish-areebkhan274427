d9e212fa048490fdaaae7648e85468811a8f2150
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-04-11 21:31:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,31 +2,36 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
-const randomTime = Math.floor(Math.random() * 4000) + 1000;
+
 var promise1=new Promise((resolve)=>{
+	const randomTime = Math.floor(Math.random() * 4000) + 1000;
 	setTimeout(()=>{
 		resolve("hello1");
-	}randomTime)
+	},randomTime)
 });
 var promise2=new Promise((resolve)=>{
+	const randomTime = Math.floor(Math.random() * 4000) + 1000;
 	setTimeout(()=>{
 		resolve("hello2");
-	}randomTime)
+	},randomTime)
 });
 var promise3=new Promise((resolve)=>{
+	const randomTime = Math.floor(Math.random() * 4000) + 1000;
 	setTimeout(()=>{
 		resolve("hello3");
-	}randomTime)
+	},randomTime)
 });
 var promise4=new Promise((resolve)=>{
+	const randomTime = Math.floor(Math.random() * 4000) + 1000;
 	setTimeout(()=>{
-		resolve("hello4);
-	}randomTime)
+		resolve("hello4");
+	},randomTime)
 });
 var promise5=new Promise((resolve)=>{
+	const randomTime = Math.floor(Math.random() * 4000) + 1000;
 	setTimeout(()=>{
 		resolve("hello5");
-	}randomTime)
+	},randomTime)
 });
 var d=document.getElementById('output');
 
